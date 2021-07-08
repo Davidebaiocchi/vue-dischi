967c823b981e3fd012a3a80139d70b51d9a3ce12
davide <davide.baiocchi97@gmail.com>
2021-07-08 14:40:15 +0200

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container-main">
+    <i class="fab fa-spotify"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .container-main {
+    background-color: hsl(210, 20.7%, 22.7%);
+    height: 50px;
+  }
+  i {
+    color: rgb(10, 214, 10);
+    font-size: 35px;
+    padding: 6px;
+    margin-left: 5px;
+  }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container-main">
+      <div class="dischi">
+          <ul>
+              <li>
+
+              </li>
+          </ul>
+      </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+
+export default {
+    name: 'Main',
+    data() {
+        return {
+            apiUrl : 'https://flynn.boolean.careers/exercises/api/array/music',
+            arrayDischi : ''
+        }
+    },
+    created() {
+        this.getDischi();
+    },
+    methods: {
+        getDischi() {
+            axios
+            .get(this.apiUrl)
+            .then(response => {
+                this.arrayDischi = response;
+                console.log(this.arrayDischi);
+            })
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .container-main {
+    background-color: hsl(209, 32.6%, 17.5%);
+    height: calc(100vh - 50px);
+    text-align: center;
+  }
+
+</style>
